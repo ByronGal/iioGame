3498b51eee9a7a4829a9344f218d5a84dfc7238e
Byron C. Gal <Byron90@gmail.com>
2013-05-05 18:32:30 -0700

    made working oscilating fire function

--- PoxBoxV2.io.js
+++ PoxBoxV2.io.js
@@ -4,7 +4,7 @@ PoxBoxV2 = function(io){
 	io.setBGColor('#424242');
 
 	//turns on Debug Console
-	///io.activateDebugger();
+	io.activateDebugger();
 
 	/*	BUG-REPORT
 	1. Drag function in the positive directions is a bit janky
@@ -15,15 +15,17 @@ PoxBoxV2 = function(io){
 	var boxX = 0; //Box x & y velocities
 	var boxY = 0; 
 
-	var topSpeed = 10; //Top speed of Player
+	var PlayerSize = 10;
+
+	var topSpeed = 6; //Top speed of Player
 
 	var speedInc = .5; //Rate velocity increments
 
 	var dragForce = .5; //Rate of velocity decrement - for now make sure it divides int speedinc evenly
 
-	var boundary = -25; //Border offset	
+	var boundary = -10; //Border offset	
 
-	var boundaryRepel = 5; //Speed  player pushed of bounda
+	var boundaryRepel = 5; //Speed player pushed off of bounds
 
 	var spacePushed = false; //Input switches
 	var leftPushed = false;
@@ -34,15 +36,21 @@ PoxBoxV2 = function(io){
 	var moveKeyXPressed = false; //Makes the drag on the X only active when a key is not pressed
 	var moveKeyYPressed = false; //Makes the drag on the Y only active when a key is not pressed
 
-	var spaceTick = 15; //Counter for canSpaceAction function
-	var spaceTimer = 120; //Length of canSpaceAction function
+	var spaceTimer = 6; //Length of canSpaceAction function
+	var spaceTick = spaceTimer; //Counter for canSpaceAction function
+	
+	var UIsize = 10; //Border offset	
 
-	//Reference variables
-	var UIsize = 25; //Border offset	
+	var laserImg = new Image();	//laser image source
+	laserImg.src = 'png/laserRed.png';
 
+	var spreadSize = 25;	//vars for bullet spreading
+	var shotInvertFlag = true;
+	var spreadInc = 0;
+	var spreadSpeed = 5;
 
 	//Spawn box playerBox
-	var playerBox = new iio.ioRect(io.canvas.center.x,io.canvas.center.y, 25)
+	var playerBox = new iio.ioRect(io.canvas.center.x,io.canvas.center.y, PlayerSize)
 		.enableKinematics()
 		.setVel(boxX,boxY)
 		.setFillStyle('white')
@@ -73,7 +81,7 @@ PoxBoxV2 = function(io){
 			});;
 
 	//Add box to io manager
-	io.addToGroup('player', playerBox);
+	io.addToGroup('player', playerBox, 1);
 
 	//debug text creation
 	var debugText1 = new iio.ioText('variable debug',500,45)	//boxX debug text
@@ -112,10 +120,14 @@ PoxBoxV2 = function(io){
 	    .setStrokeStyle('red');
 	    debugText9.font ='30px';
 	io.addToGroup('text', debugText9);
-	var debugText10 = new iio.ioText('variable debug',500,225)	//can move test debug text
+	var debugText10 = new iio.ioText('variable debug',500,225)	//shotinc debug text
 	    .setStrokeStyle('red');
 	    debugText9.font ='30px';
 	io.addToGroup('text', debugText10);
+	var debugText11 = new iio.ioText('variable debug',500,245)	//shotflag debug text
+	    .setStrokeStyle('red');
+	    debugText9.font ='30px';
+	io.addToGroup('text', debugText11);
 
 	drawUI();
 
@@ -155,6 +167,32 @@ PoxBoxV2 = function(io){
 		io.setBGColor('#'+iio.getRandomInt(0,9)+iio.getRandomInt(0,9)+iio.getRandomInt(0,9)+iio.getRandomInt(0,9)+iio.getRandomInt(0,9)+iio.getRandomInt(0,9));
 	}
 
+	function createBullletPos(x,y){
+		var shotCenter = x;
+		var shotY = y; 
+
+		if (shotInvertFlag){
+			fireBullet(shotCenter+spreadInc , shotY );
+			spreadInc += spreadSpeed;
+			if (spreadInc >= spreadSize)
+				shotInvertFlag = false;
+		}else{
+			fireBullet(shotCenter+spreadInc , shotY );
+			spreadInc -= spreadSpeed;
+			if (spreadInc <= -spreadSize)
+				shotInvertFlag = true;
+		}
+	}
+
+	fireBullet = function(x,y){	//takes the x,y from createBullletPos and makes a shot
+		 io.addToGroup('lasers', new iio.ioRect(x,y),1)
+			.createWithImage(laserImg)
+			.enableKinematics()
+			.setBound('top',0)
+			.setVel(0,boxY-6);		
+	}
+
+	//test to see if drag is needed on x axis
 	function moveKeyXPressedReleaseTest(){
 		if(leftPushed||rightPushed)
 			return true;
@@ -162,6 +200,7 @@ PoxBoxV2 = function(io){
 			return false;
 	}
 
+	//test to see if drag is needed on y axis
 	function moveKeyYPressedReleaseTest(){
 		if(upPushed||downPushed)
 			return true;
@@ -198,7 +237,8 @@ PoxBoxV2 = function(io){
 		//Spacebar action
 		if(spacePushed){
 			if(canSpaceAction()){
-				colorChange();
+				//colorChange();
+				createBullletPos(playerBox.pos.x,playerBox.pos.y);
 				spaceTick = 0;
 			}
 		};
@@ -281,7 +321,8 @@ PoxBoxV2 = function(io){
 
 	//increment timers
 	function timerIncrement(){
-		spaceTick++;
+		if(spaceTick <= spaceTimer)
+			spaceTick++;
 	}
 
 	function debugUpdate(){
@@ -294,6 +335,8 @@ PoxBoxV2 = function(io){
 		debugText7.text = 'upPushed = ' + upPushed.toString();
 		debugText8.text = 'spaceTick = ' + spaceTick.toString();
 		debugText9.text = 'moveKeyXPressed = ' + moveKeyXPressed.toString();
+		debugText10.text = 'spreadInc = ' + spreadInc.toString();
+		debugText11.text = 'shotInvertFlag = ' + shotInvertFlag.toString();
 	}
 
 	//Set update time in ms and call gameloop
